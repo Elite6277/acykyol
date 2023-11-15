15a22a191848d04b05d34b81a6a472e572e518f9
Elite6277 <hacklinux93@gmail.com>
2023-11-15 18:48:10 +0500

    acykyol_beta_version-0.1

---
+++ js/lang.js
@@ -0,0 +1,487 @@
+const langArr = {
+  "promo-title": {
+    ru: "Качественное производство",
+    en: "Quality production",
+    tm: "Berk önüm",
+  },
+  "promo-subtitle": {
+    ru: "О нас",
+    en: "About us",
+    tm: "Biz barada",
+  },
+  "promo-link-portfolio": {
+    ru: "НАША ГАЛЕРЕЯ",
+    en: "OUR GALLERY",
+    tm: "BIZIŇ GALEREÝAMYZ",
+  },
+  //   "promo-link-about": {
+  //     ru: "Про меня",
+  //     en: "About me",
+  //     tm: "Özüm barada",
+  //   },
+  "about-title": {
+    ru: "Про Нас",
+    en: "About Us",
+    tm: "Biz barada",
+  },
+  "about-subtitle": {
+    ru: "AÇYK ÝOL",
+    en: "AÇYK ÝOL",
+    tm: "AÇYK ÝOL",
+  },
+  // "about-text": {
+  //   ru: "mähriban ildeşler! Türkmenistanyň goranmak ministrliginiň buýruknamasy esasynda mary şäheriniň altyn asyr aýlaw ýolunyň ugrunda harby gullukçylara, raýat gullukçylara we raýatlarmyza niýetleNIP  gurulýan  birinji gatlary dükanly hemde hyzmat ediş nokatly, liftli ýaşaýyş jaýlar toplumyny amatly bahadan size hödürleýÄris.",
+  //   en: "mähriban ildeşler! Türkmenistanyň goranmak ministrliginiň buýruknamasy esasynda mary şäheriniň altyn asyr aýlaw ýolunyň ugrunda harby gullukçylara, raýat gullukçylara we raýatlarmyza niýetleNIP  gurulýan  birinji gatlary dükanly hemde hyzmat ediş nokatly, liftli ýaşaýyş jaýlar toplumyny amatly bahadan size hödürleýÄris.",
+  //   tm: "mähriban ildeşler! Türkmenistanyň goranmak ministrliginiň buýruknamasy esasynda mary şäheriniň altyn asyr aýlaw ýolunyň ugrunda harby gullukçylara, raýat gullukçylara we raýatlarmyza niýetleNIP  gurulýan  birinji gatlary dükanly hemde hyzmat ediş nokatly, liftli ýaşaýyş jaýlar toplumyny amatly bahadan size hödürleýÄris.",
+  // },
+  "about-second-title": {
+    ru: "Про Нас",
+    en: "About Us",
+    tm: "Biz barada",
+  },
+  "about-second-subtitle": {
+    ru: "AÇYK ÝOL",
+    en: "AÇYK ÝOL",
+    tm: "AÇYK ÝOL",
+  },
+  // "about-second-text": {
+  //   ru: "Наши сертификаты",
+  //   en: "Our sertificate",
+  //   tm: "Biziň sertifikatymyz",
+  // },
+  "sertificate-title": {
+    ru: "Наши сертификаты",
+    en: "Our sertificate",
+    tm: "Biziň sertifikatymyz",
+  },
+  "sertificate-subtitle": {
+    ru: "Наши сертификаты",
+    en: "Our sertificate",
+    tm: "Biziň sertifikatymyz",
+  },
+  "sertificate-title": {
+    ru: "Наши сертификаты",
+    en: "Our sertificate",
+    tm: "Biziň sertifikatymyz",
+  },
+  "about-third-title": {
+    ru: "Про Нас",
+    en: "About Us",
+    tm: "Biz barada",
+  },
+  "about-third-subtitle": {
+    ru: "AÇYK ÝOL",
+    en: "AÇYK ÝOL",
+    tm: "AÇYK ÝOL",
+  },
+  "about-third-text": {
+    ru: "«AÇYK ÝOL» - полипропиленовые мешки и биг-беги. Мы предлагаем биг-беги, полипропиленовые мешки. Продукция находит широкое применение в различных отраслях. Основным применением производимой упаковки является фасовка и перемещение непищевых товаров (строительные смеси, уголь и т.д.). Благодаря высокой прочности и устойчивости к внешним воздействиям, полипропиленовая упаковка обеспечивает безопасную и надежную транспортировку грузов.",
+    en: "'AÇYK ÝOL' - polypropylene bags and big bags. We offer big bags and polypropylene bags. The products are widely used in various industries. The main application of the packaging produced is the packaging and movement of non-food products (construction mixtures, coal, etc.). Due to its high strength and resistance to external influences, polypropylene packaging ensures safe and reliable transportation of goods.",
+    tm: "'AÇYK ÝOL' - polipropilen haltalar we big-beg haltalar. Biz big-beg haltalary we kiçi haltalary hödürleýäris. Önümler dürli pudaklarda giňden ulanylýar. Öndürilen gaplamanyň esasy ulanylyşy azyk däl önümleriň gaplanylmagy (gurluşyk garyndylary, kömür we ş.m.). Polipropilen haltalar ýokary güýji we daşarky täsirlere garşylygy sebäpli harytlaryň howpsuz we ygtybarly daşalmagyny üpjün edýär.",
+  },
+  "about__skills-title-web-development": {
+    ru: "Sed vitae nunc",
+    en: "Sed vitae nunc",
+    tm: "Sed vitae nunc",
+  },
+  our_products__title: {
+    ru: "НАШИ ПРОДУКТЫ",
+    en: "OUR PRODUCTS",
+    tm: "ÖNÜMLERIMIZ",
+  },
+  "our_products-potfolio__subtitle": {
+    ru: "НАШИ ПРОДУКТЫ",
+    en: "OUR PRODUCTS",
+    tm: "ÖNÜMLERIMIZ",
+  },
+  "about__skills-text-web-development": {
+    ru: "Sed vitae nunc vitae massa varius aliquam sit amet id ante. Maecenas at accumsan quam. Proin quis enim vulputate, suscipit dui non, luctus justo.",
+    en: "Sed vitae nunc vitae massa varius aliquam sit amet id ante. Maecenas at accumsan quam. Proin quis enim vulputate, suscipit dui non, luctus justo.",
+    tm: "Sed vitae nunc vitae massa varius aliquam sit amet id ante. Maecenas at accumsan quam. Proin quis enim vulputate, suscipit dui non, luctus justo..",
+  },
+  "about__skills-title-application-development": {
+    ru: "Sed vitae nunc vitae massa",
+    en: "Sed vitae nunc vitae massa",
+    tm: "Sed vitae nunc vitae massa",
+  },
+  "about__skills-text-application-development": {
+    ru: "Sed vitae nunc vitae massa varius aliquam sit amet id ante. Maecenas at accumsan quam. Proin quis enim vulputate, suscipit dui non, luctus justo.",
+    en: "Sed vitae nunc vitae massa varius aliquam sit amet id ante. Maecenas at accumsan quam. Proin quis enim vulputate, suscipit dui non, luctus justo..",
+    tm: "Sed vitae nunc vitae massa varius aliquam sit amet id ante. Maecenas at accumsan quam. Proin quis enim vulputate, suscipit dui non, luctus justo.",
+  },
+  "about__skills-title-design": {
+    ru: "Sed vitae nunc",
+    en: "Sed vitae nunc",
+    tm: "Sed vitae nunc",
+  },
+  "about__skills-text-design": {
+    ru: "Sed vitae nunc vitae massa varius aliquam sit amet id ante. Maecenas at accumsan quam. Proin quis enim vulputate, suscipit dui non",
+    en: "Sed vitae nunc vitae massa varius aliquam sit amet id ante. Maecenas at accumsan quam. Proin quis enim vulputate, suscipit dui non",
+    tm: "Sed vitae nunc vitae massa varius aliquam sit amet id ante. Maecenas at accumsan quam. Proin quis enim vulputate, suscipit dui non",
+  },
+  "resume-experience": {
+    ru: "Опыт",
+    en: "Experience",
+    tm: "Tejribe",
+  },
+  "resume-help": {
+    ru: "Sed vitae nunc",
+    en: "Sed vitae nunc",
+    tm: "Sed vitae nunc",
+  },
+  "resume-title-education": {
+    ru: "Sed vitae",
+    en: "Sed vitae",
+    tm: "Sed vitae",
+  },
+  "resume-title-education-institute": {
+    ru: " Vitae",
+    en: " Vitae",
+    tm: " Vitae",
+  },
+  "resume-education-location-institute": {
+    ru: "Sed vitae nunc | Sed vitae nunc vitae massa varius aliquam sit amet id ante",
+    en: "Sed vitae nunc | Sed vitae nunc vitae massa varius aliquam sit amet id ante",
+    tm: "Sed vitae nunc | Sed vitae nunc vitae massa varius aliquam sit amet id ante",
+  },
+  "resume-title-course": {
+    ru: "Vitae",
+    en: "Vitae",
+    tm: "Vitae",
+  },
+  "resume-subtitle-course": {
+    ru: "Sed vitae nunc vitae massa varius",
+    en: "Sed vitae nunc vitae massa varius",
+    tm: "Sed vitae nunc vitae massa varius",
+  },
+  "resume-experience-work": {
+    ru: "Sed vitae",
+    en: "Sed vitae",
+    tm: "Sed vitae",
+  },
+  "resume-title-experience-frontend": {
+    ru: "Sed vitae nunc",
+    en: "Sed vitae nunc",
+    tm: "Sed vitae nunc",
+  },
+  "resume-subtitle-experience-frontend": {
+    ru: "Green-code | Sed vitae nunc",
+    en: "Green-code | Sed vitae nunc",
+    tm: "Green-code | Sed vitae nunc",
+  },
+  "resume-title-experience-design": {
+    ru: "Sed vitae nunc",
+    en: "Sed vitae nunc",
+    tm: "Sed vitae nunc",
+  },
+  "resume-subtitle-experience-design": {
+    ru: "Green-code | Sed vitae nunc",
+    en: "Green-code | Sed vitae nunc",
+    tm: "Green-code | Sed vitae nunc",
+  },
+  "title__section-title": {
+    ru: "Навыки",
+    en: "My Skills",
+    tm: "Başarnyklar",
+  },
+  "title__section-subtitle": {
+    ru: "Sed vitae nunc",
+    en: "Sed vitae nunc",
+    tm: "Sed vitae nunc",
+  },
+  "skills__item-body-html": {
+    ru: "Maecenas vestibulum libero turpis, eu efficitur eros vehicula a. Duis fermentum fermentum tortor semper vulputate.",
+    en: "Maecenas vestibulum libero turpis, eu efficitur eros vehicula a. Duis fermentum fermentum tortor semper vulputate.",
+    tm: "Maecenas vestibulum libero turpis, eu efficitur eros vehicula a. Duis fermentum fermentum tortor semper vulputate.",
+  },
+  "skills__item-body-css": {
+    ru: "Maecenas vestibulum libero turpis, eu efficitur eros vehicula a. Duis fermentum fermentum tortor semper vulputate.",
+    en: "Maecenas vestibulum libero turpis, eu efficitur eros vehicula a. Duis fermentum fermentum tortor semper vulputate.",
+    tm: "Maecenas vestibulum libero turpis, eu efficitur eros vehicula a. Duis fermentum fermentum tortor semper vulputate.",
+  },
+  "skills__item-body-bootstrap": {
+    ru: "Maecenas vestibulum libero turpis, eu efficitur eros vehicula a. Duis fermentum fermentum tortor semper vulputate.",
+    en: "Maecenas vestibulum libero turpis, eu efficitur eros vehicula a. Duis fermentum fermentum tortor semper vulputate.",
+    tm: "Maecenas vestibulum libero turpis, eu efficitur eros vehicula a. Duis fermentum fermentum tortor semper vulputate.",
+  },
+  "skills__item-body-javascript": {
+    ru: "Maecenas vestibulum libero turpis, eu efficitur eros vehicula a. Duis fermentum fermentum tortor semper vulputate.",
+    en: "Maecenas vestibulum libero turpis, eu efficitur eros vehicula a. Duis fermentum fermentum tortor semper vulputate.",
+    tm: "Maecenas vestibulum libero turpis, eu efficitur eros vehicula a. Duis fermentum fermentum tortor semper vulputate.",
+  },
+  "skills__item-body-jquery": {
+    ru: "Maecenas vestibulum libero turpis, eu efficitur eros vehicula a. Duis fermentum fermentum tortor semper vulputate.",
+    en: "Maecenas vestibulum libero turpis, eu efficitur eros vehicula a. Duis fermentum fermentum tortor semper vulputate.",
+    tm: "Maecenas vestibulum libero turpis, eu efficitur eros vehicula a. Duis fermentum fermentum tortor semper vulputate.",
+  },
+  "skills__item-body-react": {
+    ru: "Maecenas vestibulum libero turpis, eu efficitur eros vehicula a. Duis fermentum fermentum tortor semper vulputate.",
+    en: "Maecenas vestibulum libero turpis, eu efficitur eros vehicula a. Duis fermentum fermentum tortor semper vulputate.",
+    tm: "Maecenas vestibulum libero turpis, eu efficitur eros vehicula a. Duis fermentum fermentum tortor semper vulputate.",
+  },
+  "skills__item-body-nodejs": {
+    ru: "Maecenas vestibulum libero turpis, eu efficitur eros vehicula a. Duis fermentum fermentum tortor semper vulputate.",
+    en: "Maecenas vestibulum libero turpis, eu efficitur eros vehicula a. Duis fermentum fermentum tortor semper vulputate.",
+    tm: "Maecenas vestibulum libero turpis, eu efficitur eros vehicula a. Duis fermentum fermentum tortor semper vulputate.",
+  },
+  "skills__item-body-postgresql": {
+    ru: "Maecenas vestibulum libero turpis, eu efficitur eros vehicula a. Duis fermentum fermentum tortor semper vulputate.",
+    en: "Maecenas vestibulum libero turpis, eu efficitur eros vehicula a. Duis fermentum fermentum tortor semper vulputate.",
+    tm: "Maecenas vestibulum libero turpis, eu efficitur eros vehicula a. Duis fermentum fermentum tortor semper vulputate.",
+  },
+  "skills__ratings-title-creating-sites": {
+    ru: "Maecenas vestibulum",
+    en: "Maecenas vestibulum",
+    tm: "Maecenas vestibulum",
+  },
+  "skills__ratings-title-creative": {
+    ru: "Maecenas vestibulum",
+    en: "Maecenas vestibulum",
+    tm: "Maecenas vestibulum",
+  },
+  "skills__ratings-title-creating-apps": {
+    ru: "Maecenas vestibulum",
+    en: "Maecenas vestibulum",
+    tm: "Maecenas vestibulum",
+  },
+  "skills__ratings-title-design": {
+    ru: "Maecenas vestibulum",
+    en: "Maecenas vestibulum",
+    tm: "Maecenas vestibulum",
+  },
+  "skills__ratings-title-data": {
+    ru: "Maecenas vestibulum",
+    en: "Maecenas vestibulum",
+    tm: "Maecenas vestibulum",
+  },
+  "skills__ratings-title-creating-sites": {
+    ru: "Maecenas vestibulum",
+    en: "Maecenas vestibulum",
+    tm: "Maecenas vestibulum",
+  },
+  potfolio__title: {
+    ru: "НАША ГАЛЕРЕЯ",
+    en: "OUR GALLERY",
+    tm: "BIZIŇ GALEREÝAMYZ",
+  },
+  potfolio__subtitle: {
+    ru: "НАША ГАЛЕРЕЯ",
+    en: "OUR GALLERY",
+    tm: "BIZIŇ GALEREÝAMYZ",
+  },
+  price__title: {
+    ru: "Прайс-лист",
+    en: "Priсe-list",
+    tm: "Bahalar",
+  },
+  "landing-price__count": {
+    ru: "от 8000 TMT.",
+    en: "8000 TMT.",
+    tm: "8000 TMT.",
+  },
+  "landing-price__descr": {
+    ru: "Maecenas vestibulum libero turpis, eu efficitur eros vehicula a.",
+    en: "Maecenas vestibulum libero turpis, eu efficitur eros vehicula a..",
+    tm: "Maecenas vestibulum libero turpis, eu efficitur eros vehicula a.",
+  },
+  "webapp-price__type": {
+    ru: "Web-приложение",
+    en: "Web application",
+    tm: "Web programmasy",
+  },
+  "webapp-price__count": {
+    ru: "от 28000 TMT.",
+    en: "28000 TMT.",
+    tm: "28000 TMT.",
+  },
+  "webapp-price__decsr": {
+    ru: "Приложение внутри браузера для лучшего комфорта",
+    en: "App inside the browser for better comfort",
+    tm: "Programmaňyz brauzeriň içinde siziň rahatlygyňyz uçin",
+  },
+  "corporate-price__type": {
+    ru: "Корпоративный сайт",
+    en: "Corporate website",
+    tm: "Korporatiw website",
+  },
+  "corporate-price__count": {
+    ru: "от 20000 TMT.",
+    en: "20000 TMT.",
+    tm: "20000 TMT.",
+  },
+  "corporate-price__decsr": {
+    ru: "Сайт для вашего бизнеса или компании",
+    en: "Website for your business or company",
+    tm: "Işiňiz ýa-da kompaniýaňyz üçin web sahypa",
+  },
+  "mobileapp-price__type": {
+    ru: "Android/IOS Приложение",
+    en: "Android/IOS Application",
+    tm: "Android/IOS Programmasy ",
+  },
+  "mobileapp-price__count": {
+    ru: "от 25000 TMT.",
+    en: "25000 TMT.",
+    tm: "25000 TMT.",
+  },
+  "mobileapp-price__decsr": {
+    ru: "Приложение для смартфона любой ОС",
+    en: "Smartphone app for any OS",
+    tm: "Islendik OS üçin smartfon programmasy",
+  },
+  "onlineshop-price__type": {
+    ru: "Интернет-магазин",
+    en: "Online-shop",
+    tm: "Online-market",
+  },
+  "onlineshop-price__count": {
+    ru: "от 30000 TMT.",
+    en: "30000 TMT.",
+    tm: "30000 TMT.",
+  },
+  "onlineshop-price__decsr": {
+    ru: "Инструмент для ваших продаж в сети",
+    en: "A tool for your online sales",
+    tm: "Onlaýn satuwyňyz üçin gural",
+  },
+  "design-price__type": {
+    ru: "Дизайн",
+    en: "Design",
+    tm: "Dezaýn",
+  },
+  "design-price__count": {
+    ru: "Индивидуально.",
+    en: "Individually.",
+    tm: "Aýratynlykda.",
+  },
+  "design-price__decsr": {
+    ru: "Создам дизайн для вашего сайта/приложения...",
+    en: "I will create a design for your website/application...",
+    tm: "Web sahypaňyz / programmaňyz üçin dizaýn dörederin ...",
+  },
+  contacts__title: {
+    ru: "Cвязаться с нами",
+    en: "Contact us",
+    tm: "Habarlaşmak",
+  },
+  contacts__subtitle: {
+    ru: "Свяжитесь с нами",
+    en: "Contact with us",
+    tm: "Habarlaşmak üçin",
+  },
+  contacts__text: {
+    ru: "Любым удобным для вас способом:",
+    en: "In any way convenient for you:",
+    tm: "Islendik görnüşde:",
+  },
+  "contacts__text-advice": {
+    ru: "Или оставьте ваши данныe:",
+    en: "Or leave your details and we will write to you:",
+    tm: "Ýa-da hat ugradyň, biz siz bilen habarlaşarys:",
+  },
+  label__name: {
+    ru: "Ваше имя",
+    en: "Your name",
+    tm: "Siziň adyňyz",
+  },
+  label__email: {
+    ru: "Ваша почта",
+    en: "Your email",
+    tm: "Elektron salgyňyz",
+  },
+  label__text: {
+    ru: "Ваше сообщение",
+    en: "Your message",
+    tm: "Hatyň teksti",
+  },
+  contacts__send: {
+    ru: "Отправить сообщение",
+    en: "Send message",
+    tm: "Haty ibermek",
+  },
+  privacy__policy: {
+    ru: "Я согласен(а) с",
+    en: "I agree to the",
+    tm: "Men razy",
+  },
+  "privacy__policy-link": {
+    ru: "политикой конфиденциальности",
+    en: "privacy policy",
+    tm: "gizlinlik syýasaty",
+  },
+  modal__subtitle: {
+    ru: "Спасибо за вашу заявку!",
+    en: "Thanks for your submission!",
+    tm: "Sag boluň!",
+  },
+  modal__descr: {
+    ru: "Мы свяжемся с вами в ближайшее время!",
+    en: "We will contact you shortly!",
+    tm: "Biz siz bilen gysga wagtda habarlaşarys!",
+  },
+  "menu__link-about": {
+    ru: "О НАС",
+    en: "ABOUT US",
+    tm: "BIZ BARADA",
+  },
+  "menu__link-resume": {
+    ru: "НАШИ ПРОДУКТЫ",
+    en: "OUR PRODUCTS",
+    tm: "ÖNÜMLERIMIZ",
+  },
+  "menu__link-skills": {
+    ru: "МОИ НАВЫКИ",
+    en: "MY SKILLS",
+    tm: "BAŞARNYKLAR",
+  },
+  "menu__link-portfolio": {
+    ru: "НАША ГАЛЕРЕЯ",
+    en: "OUR GALLERY",
+    tm: "BIZIŇ GALEREÝAMYZ",
+  },
+  "menu__link-price": {
+    ru: "ПРАЙС-ЛИСТ",
+    en: "PRICE-LIST",
+    tm: "BAHALAR",
+  },
+  "menu__link-contacts": {
+    ru: "КОНТАКТЫ",
+    en: "CONTACTS",
+    tm: "HABARLAŞMAK",
+  },
+  "nav__link-about": {
+    ru: "О НАС",
+    en: "ABOUT US",
+    tm: "BIZ BARADA",
+  },
+  "nav__link-resume": {
+    ru: "НАША ПРОДУКЦИЯ",
+    en: "OUR PRODUCTS",
+    tm: "ÖNÜMLERIMIZ",
+  },
+  "nav__link-skills": {
+    ru: "МОИ НАВЫКИ",
+    en: "MY SKILLS",
+    tm: "BAŞARNYKLAR",
+  },
+  "nav__link-portfolio": {
+    ru: "НАША ГАЛЕРЕЯ",
+    en: "OUR GALLERY",
+    tm: "BIZIŇ GALEREÝAMYZ",
+  },
+  "nav__link-price": {
+    ru: "ПРАЙС-ЛИСТ",
+    en: "PRICE-LIST",
+    tm: "BAHALAR",
+  },
+  "nav__link-contacts": {
+    ru: "КОНТАКТЫ",
+    en: "CONTACTS",
+    tm: "HABARLAŞMAK",
+  },
+  "about-company": {
+    ru: "Права защищены. © 2023 achykyol@sanly.tm",
+    en: "All rights reserved. © 2023 achykyol@sanly.tm",
+    tm: "Ähli hukuklary goralan. © 2023 achykyol@sanly.tm",
+  },
+};
---
+++ js/script.js
@@ -0,0 +1,184 @@
+//Preloader
+window.onload = function () {
+  document.body.classList.add("loaded_hiding");
+  window.setTimeout(function () {
+    document.body.classList.add("loaded");
+    document.body.classList.remove("loaded_hiding");
+  }, 500);
+};
+
+// Работа с шапкой при скроле
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function () {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "-0px";
+  } else {
+    document.getElementById("navbar").style.top = "-72px";
+  }
+  prevScrollpos = currentScrollPos;
+};
+//Menu burger
+const burger = document.querySelector(".hamburger"),
+  menu = document.querySelector(".menu"),
+  menuLinks = document.querySelectorAll(".menu__link"),
+  closeElem = document.querySelector(".menu__close"),
+  overlay = document.querySelector(".menu__overlay");
+
+burger.addEventListener("click", () => {
+  menu.classList.toggle("active");
+  burger.classList.toggle("active");
+});
+//Код закрывет меню при нажатии на ссылку
+menuLinks.forEach((link) =>
+  link.addEventListener("click", () => {
+    menu.classList.remove("active");
+    burger.classList.remove("active");
+  })
+);
+//код который закрывает меню при нажатии на крестик
+closeElem.addEventListener("click", () => {
+  menu.classList.remove("active");
+});
+//код который закрывает меню при нажатии вне меню
+overlay.addEventListener("click", () => {
+  menu.classList.remove("active");
+});
+
+//Интерактив для линии
+const counters = document.querySelectorAll(".skills__ratings-counter"),
+  lines = document.querySelectorAll(".skills__ratings-line span");
+
+counters.forEach((item, i) => {
+  lines[i].style.width = item.innerHTML;
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  new WOW().init();
+});
+
+//Modal
+
+$("[data-modal=consultation]").on("click", function () {
+  $(".overlay, #consultation").fadeIn("slow");
+});
+$(".modal__close").on("click", function () {
+  $(".overlay, #consultation, #order, #thanks").fadeOut("slow");
+});
+$(".button_mini").each(function (i) {
+  $(this).on("click", function () {
+    $("#order .modal__descr").text($(".catalog-item__subtitle").eq(i).text());
+    $(".overlay, #order").fadeIn("slow");
+  });
+});
+
+//Php
+$("form").submit(function (e) {
+  e.preventDefault();
+
+  //if (!$(this).valid()) {
+  //   return;
+  //}
+
+  $.ajax({
+    type: "POST",
+    url: "mailer/smart.php",
+    data: $(this).serialize(),
+  }).done(function () {
+    $(this).find("input").val("");
+    $("#consultation, #order").fadeOut();
+    $(".overlay, #thanks").fadeIn("slow");
+
+    $("form").trigger("reset");
+  });
+  return false;
+});
+
+//Плавный скрол
+$(window).scroll(function () {
+  if ($(this).scrollTop() > 1600) {
+    $(".pageup").fadeIn();
+  } else {
+    $(".pageup").fadeOut();
+  }
+});
+//$(window).scroll(function () {
+//   if ($(this).scrollTop() < 3800) {
+//      $('.promo__call').fadeIn();
+//   } else {
+//      $('.promo__call').fadeOut();
+//   }
+//});
+
+$("a[href=#up]").click(function () {
+  const _href = $(this).attr("href");
+  $("html, body").animate({ scrollTop: $(_href).offset().top + "px" });
+  return false;
+});
+//$("a[href=#price]").click(function () {
+//   const _href = $(this).attr("href");
+//   $("html, body").animate({ scrollTop: $(_href).offset().top + "px" });
+//   return false;
+//});
+
+//Themes
+var btn = document.getElementById("theme-button");
+var link = document.getElementById("theme-link");
+
+btn.addEventListener("click", function () {
+  ChangeTheme();
+});
+
+function ChangeTheme() {
+  let lightTheme = "css/style.min.css";
+  let darkTheme = "css/dark.min.css";
+
+  var currTheme = link.getAttribute("href");
+  var theme = "";
+
+  if (currTheme == lightTheme) {
+    currTheme = darkTheme;
+    theme = "dark";
+  } else {
+    currTheme = lightTheme;
+    theme = "style";
+  }
+
+  link.setAttribute("href", currTheme);
+
+  //Save(theme);
+}
+
+//Change lang
+
+const select = document.querySelector("select");
+const allLang = ["en", "ru", "tm"];
+
+select.addEventListener("change", changeURLLanguage);
+//Перенаправить на url c указанием языка
+
+function changeURLLanguage() {
+  let lang = select.value;
+  location.href = window.location.pathname + "#" + lang;
+  location.reload();
+}
+
+function changeLanguage() {
+  let hash = window.location.hash;
+  hash = hash.substring(1);
+  console.log(hash);
+  if (!allLang.includes(hash)) {
+    location.href = window.location.pathname + "#en";
+    location.reload();
+  }
+  select.value = hash;
+  //document.querySelector('title').innerHTML = langArr['unit'][hash];
+  for (let key in langArr) {
+    let elem = document.querySelector(".lng-" + key);
+    if (elem) {
+      elem.innerHTML = langArr[key][hash];
+    }
+  }
+}
+
+changeLanguage();
