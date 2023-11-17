41e2a8a6c4768ad95d88dd9a802d32527e21c92f
Elite6277 <hacklinux93@gmail.com>
2023-11-18 00:20:08 +0500

    acykyol_beta_version-0.3

--- js/lang.js
+++ js/lang.js
@@ -5,9 +5,9 @@ const langArr = {
     tm: "Berk önüm",
   },
   "promo-subtitle": {
-    ru: "О нас",
-    en: "About us",
-    tm: "Biz barada",
+    ru: "О ЗАВОДЕ",
+    en: "ABOUT FACTORY",
+    tm: "ZAWOD BARADA",
   },
   "promo-link-portfolio": {
     ru: "НАША ГАЛЕРЕЯ",
@@ -20,24 +20,24 @@ const langArr = {
   //     tm: "Özüm barada",
   //   },
   "about-title": {
-    ru: "Про Нас",
-    en: "About Us",
-    tm: "Biz barada",
+    ru: "О ЗАВОДЕ",
+    en: "ABOUT FACTORY",
+    tm: "ZAWOD BARADA",
   },
   "about-subtitle": {
     ru: "AÇYK ÝOL",
     en: "AÇYK ÝOL",
     tm: "AÇYK ÝOL",
   },
-  // "about-text": {
-  //   ru: "mähriban ildeşler! Türkmenistanyň goranmak ministrliginiň buýruknamasy esasynda mary şäheriniň altyn asyr aýlaw ýolunyň ugrunda harby gullukçylara, raýat gullukçylara we raýatlarmyza niýetleNIP  gurulýan  birinji gatlary dükanly hemde hyzmat ediş nokatly, liftli ýaşaýyş jaýlar toplumyny amatly bahadan size hödürleýÄris.",
-  //   en: "mähriban ildeşler! Türkmenistanyň goranmak ministrliginiň buýruknamasy esasynda mary şäheriniň altyn asyr aýlaw ýolunyň ugrunda harby gullukçylara, raýat gullukçylara we raýatlarmyza niýetleNIP  gurulýan  birinji gatlary dükanly hemde hyzmat ediş nokatly, liftli ýaşaýyş jaýlar toplumyny amatly bahadan size hödürleýÄris.",
-  //   tm: "mähriban ildeşler! Türkmenistanyň goranmak ministrliginiň buýruknamasy esasynda mary şäheriniň altyn asyr aýlaw ýolunyň ugrunda harby gullukçylara, raýat gullukçylara we raýatlarmyza niýetleNIP  gurulýan  birinji gatlary dükanly hemde hyzmat ediş nokatly, liftli ýaşaýyş jaýlar toplumyny amatly bahadan size hödürleýÄris.",
-  // },
+  "about-text": {
+    ru: "О КОМПАНИИ <br/><br/> МЕШКИ И БИГ-БЭГ МЕШКИ ПОЛИПРОПИЛЕНОВЫЕ ОПТОМ ОТ ПРОИЗВОДИТЕЛЯ <br/> AÇYK ÝOL — крупный инновационный национальный производитель полипропиленовых мешков с собственной лабораторией качества в ТУРКМЕНИСТАНЕ. <br/> Компания производит более 7.5 тонн руллонов для производства мешков используя лучшее оборудование от компании YongMing. <br/><br/> Наша миссия<br/>  <br/>Мы создаём лучшие упаковочные и перевозочные решения, которые помогают нашим партнёрам и их клиентам развиваться и добиваться успеха в бизнесе используя инновации в упаковке и перевозке. <br/><br/> Наши ценности <br/> <br/>— Профессионализм<br/>— Адаптированность и способность к усовершенствованию<br/>— Ориентация на результат<br/>— Открытость<br/>— обеспечивает входной контроль сырья<br/>— следит за постоянством рецептуры сырья<br/>— следит за плотностью плетения рукава<br/>— следит за прочностью готового полипропиленовых Мешков<br/><br/>На сегодня, мы построили долгосрочные партнёрские отношения более чем с 500 клиентами.",
+    en: "ABOUT THE COMPANY <br/><br/> POLYPROPYLENE BAGS AND BIG BAGS WHOLESALE FROM THE MANUFACTURER <br/> AÇYK ÝOL is a large innovative national manufacturer of polypropylene bags with its own quality laboratory in TURKMENISTAN. <br/> The company produces more than 7.5 tons of rolls for the production of bags using the best equipment from YongMing. <br/><br/> Our Mission<br/> <br/>We create the best packaging and shipping solutions that help our partners and their clients grow and achieve business success using innovations in packaging and shipping. <br/><br/> Our values <br/> <br/>— Professionalism<br/>— Adaptability and ability to improve<br/>— Result orientation<br/>— Openness<br/>— ensures incoming control of raw materials<br/>—monitors the consistency of the raw material recipe<br/>—monitors the density of the weaving of the sleeves<br/>—monitors the strength of the finished polypropylene bags<br/><br/>Today, we have built long-term partnerships with more than 500 clients.",
+    tm: "KOMPANIÝA  BARADA <br/><br/> POLIPROPYLEN HALTALAR WE BIG-BEG HALTALAR <br/> AÇYK ÝOL, TÜRKMENISTANDA öz hil barlaghanasy bolan polipropilen haltalary öndürýän iri innowasiýa milli öndüriji.Kompaniýa, “YongMing” -iň iň oňat enjamlaryny ulanyp, halta öndürmek üçin 7,5 tonnadan gowrak rulon öndürýär.<br/><br/>Biziň wezipämiz <br/><br/> Hyzmatdaşlarymyza we müşderilerine gaplamakda we daşamakda täzelikleri ulanyp ösmäge we işde üstünlik gazanmaga kömek edýän iň oňat gaplama we iberiş çözgütlerini döredýäris.<br/><br/>Biziň gymmatlyklarymyz<br/><br/>- Professionalizm<br/>- Uýgunlaşmak we gowulaşdyrmak ukyby<br/>- Netijelere gönükdirilen<br/>- Açyklyk<br/>- çig malyň gelýän gözegçiligini üpjün edýär<br/>- çig mal reseptiniň yzygiderliligine gözegçilik edýär<br/>- ýeň dokamagyň berkligine gözegçilik edýär<br/>- taýýar polipropilen haltalaryň berkligine gözegçilik edýär<br/><br/>Häzirki wagtda 500-den gowrak müşderi bilen uzak möhletli hyzmatdaşlygy gurduk.",
+  },
   "about-second-title": {
-    ru: "Про Нас",
-    en: "About Us",
-    tm: "Biz barada",
+    ru: "О ЗАВОДЕ",
+    en: "ABOUT FACTORY",
+    tm: "ZAWOD BARADA",
   },
   "about-second-subtitle": {
     ru: "AÇYK ÝOL",
@@ -46,28 +46,28 @@ const langArr = {
   },
   // "about-second-text": {
   //   ru: "Наши сертификаты",
-  //   en: "Our sertificate",
-  //   tm: "Biziň sertifikatymyz",
+  //   en: "OUR CERTIFICATE",
+  //   tm: "Şahadatnamalarymyz",
   // },
   "sertificate-title": {
-    ru: "Наши сертификаты",
-    en: "Our sertificate",
-    tm: "Biziň sertifikatymyz",
+    ru: "НАШИ СЕРТИФИКАТЫ",
+    en: "OUR CERTIFICATE",
+    tm: "ŞAHADATNAMALARYMYZ",
   },
   "sertificate-subtitle": {
-    ru: "Наши сертификаты",
-    en: "Our sertificate",
-    tm: "Biziň sertifikatymyz",
+    ru: "НАШИ СЕРТИФИКАТЫ",
+    en: "OUR CERTIFICATE",
+    tm: "ŞAHADATNAMALARYMYZ",
   },
   "sertificate-title": {
-    ru: "Наши сертификаты",
-    en: "Our sertificate",
-    tm: "Biziň sertifikatymyz",
+    ru: "НАШИ СЕРТИФИКАТЫ",
+    en: "OUR CERTIFICATE",
+    tm: "ŞAHADATNAMALARYMYZ",
   },
   "about-third-title": {
-    ru: "Про Нас",
-    en: "About Us",
-    tm: "Biz barada",
+    ru: "О ЗАВОДЕ",
+    en: "ABOUT FACTORY",
+    tm: "ZAWOD BARADA",
   },
   "about-third-subtitle": {
     ru: "AÇYK ÝOL",
@@ -180,9 +180,9 @@ const langArr = {
     tm: "Green-code | Sed vitae nunc",
   },
   "title__section-title": {
-    ru: "Навыки",
-    en: "My Skills",
-    tm: "Başarnyklar",
+    ru: "НАШИ СЕРТИФИКАТЫ",
+    en: "OUR CERTIFICATE",
+    tm: "ŞAHADATNAMALARYMYZ",
   },
   "title__section-subtitle": {
     ru: "Sed vitae nunc",
@@ -420,9 +420,9 @@ const langArr = {
     tm: "Biz siz bilen gysga wagtda habarlaşarys!",
   },
   "menu__link-about": {
-    ru: "О НАС",
-    en: "ABOUT US",
-    tm: "BIZ BARADA",
+    ru: "О ЗАВОДЕ",
+    en: "ABOUT FACTORY",
+    tm: "ZAWOD BARADA",
   },
   "menu__link-resume": {
     ru: "НАШИ ПРОДУКТЫ",
@@ -430,9 +430,9 @@ const langArr = {
     tm: "ÖNÜMLERIMIZ",
   },
   "menu__link-skills": {
-    ru: "МОИ НАВЫКИ",
-    en: "MY SKILLS",
-    tm: "BAŞARNYKLAR",
+    ru: "НАШИ СЕРТИФИКАТЫ",
+    en: "OUR CERTIFICATE",
+    tm: "ŞAHADATNAMALARYMYZ",
   },
   "menu__link-portfolio": {
     ru: "НАША ГАЛЕРЕЯ",
@@ -450,9 +450,9 @@ const langArr = {
     tm: "HABARLAŞMAK",
   },
   "nav__link-about": {
-    ru: "О НАС",
-    en: "ABOUT US",
-    tm: "BIZ BARADA",
+    ru: "О ЗАВОДЕ",
+    en: "ABOUT FACTORY",
+    tm: "ZAWOD BARADA",
   },
   "nav__link-resume": {
     ru: "НАША ПРОДУКЦИЯ",
@@ -460,9 +460,9 @@ const langArr = {
     tm: "ÖNÜMLERIMIZ",
   },
   "nav__link-skills": {
-    ru: "МОИ НАВЫКИ",
-    en: "MY SKILLS",
-    tm: "BAŞARNYKLAR",
+    ru: "НАШИ СЕРТИФИКАТЫ",
+    en: "OUR CERTIFICATE",
+    tm: "ŞAHADATNAMALARYMYZ",
   },
   "nav__link-portfolio": {
     ru: "НАША ГАЛЕРЕЯ",
@@ -480,8 +480,8 @@ const langArr = {
     tm: "HABARLAŞMAK",
   },
   "about-company": {
-    ru: "Права защищены. © 2023 achykyol@sanly.tm",
-    en: "All rights reserved. © 2023 achykyol@sanly.tm",
-    tm: "Ähli hukuklary goralan. © 2023 achykyol@sanly.tm",
+    ru: "Права защищены. © 2023 AÇYK ÝOL",
+    en: "All rights reserved. © 2023 AÇYK ÝOL",
+    tm: "Ähli hukuklary goralan. © 2023 AÇYK ÝOL",
   },
 };
